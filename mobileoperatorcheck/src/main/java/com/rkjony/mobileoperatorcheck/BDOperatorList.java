dbb10fd8763ccf48a0422edcba699413b74db526
Rezaul Karim <rezaulkarim.imraan@gmail.com>
2019-09-25 13:59:45 +0600

    bangladesh operator list added

---
+++ mobileoperatorcheck/src/main/java/com/rkjony/mobileoperatorcheck/BDOperatorList.java
@@ -0,0 +1,10 @@
+package com.rkjony.mobileoperatorcheck;
+
+public class BDOperatorList {
+    public static final String GrameenPhone = "GrameenPhone";
+    public static final String Robi = "Robi";
+    public static final String Banglalink = "Banglalink";
+    public static  final String Airtel = "Airtel";
+    public static  final String Teletalk = "Teletalk";
+    public static  final  String Others = "Others";
+}
--- mobileoperatorcheck/src/main/java/com/rkjony/mobileoperatorcheck/OperatorCheckBD.java
+++ mobileoperatorcheck/src/main/java/com/rkjony/mobileoperatorcheck/OperatorCheckBD.java
@@ -4,17 +4,17 @@ public class OperatorCheckBD {
     public String checkWithPhone(String phoneNumber){
 
         if (phoneNumber.startsWith("+88018") || phoneNumber.startsWith("018") ){
-            return "Robi";
+            return BDOperatorList.Robi;
         }else if(phoneNumber.startsWith("+88017") || phoneNumber.startsWith("017") || phoneNumber.startsWith("+88013") || phoneNumber.startsWith("013")){
-            return "GrameenPhone";
+            return BDOperatorList.GrameenPhone;
         }else if (phoneNumber.startsWith("+88016") || phoneNumber.startsWith("016")){
-            return "Airtel";
+            return BDOperatorList.Airtel;
         }else if (phoneNumber.startsWith("+88019") || phoneNumber.startsWith("019") || phoneNumber.startsWith("+88014") || phoneNumber.startsWith("014")){
-            return "Banglalink";
+            return BDOperatorList.Banglalink;
         }else if (phoneNumber.startsWith("+88015") || phoneNumber.startsWith("015")){
-            return "Teletalk";
+            return BDOperatorList.Teletalk;
         }else {
-            return "Other";
+            return BDOperatorList.Others;
         }
 
 
